bd3db6e9065cd35063f6240736d4cd0b28d89bbf
Philippe Marchand <PHMAR122@ulaval.ca>
2025-04-16 19:53:28 -0400

    fichier equipe exact + simulation

--- Equipe331.py
+++ Equipe331.py
@@ -3,7 +3,20 @@ import matplotlib.pyplot as plt
 from problimite import solve_boundary_problem
 
 
-def y_exact(x):
+def exact(x):
     c = 0.4
     d = 0.81
     return (c - 0.4 / x**2) - (c - 0.4 / d) * np.log(x) / np.log(0.9)
+
+def simulation(h):
+    a, b = 0.9, 1.0
+
+    N = int(round((b - a) / h))
+    x = np.linspace(a, b, N + 1)
+    P = -1.0 / x
+    Q = np.zeros_like(x)
+    R = -1.6 / x**4
+    y = solve_boundary_problem((b - a) / N, P, Q, R, a, b, alpha=0.0, beta=0.0)
+    return x, y
+
+
