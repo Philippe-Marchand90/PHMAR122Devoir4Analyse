51d67cc1433b255746c507376d1db33032d9a362
Philippe Marchand <PHMAR122@ulaval.ca>
2025-04-16 20:18:25 -0400

    ajustement figure 2.1

--- Equipe331.py
+++ Equipe331.py
@@ -65,12 +65,10 @@ if __name__ == '__main__':
     plt.title('Figure 2 - Erreur en fonction de h', fontsize=14, fontweight='bold')
     plt.xlabel('h')
     plt.ylabel('E(h)')
-    plt.xlim(1e-5, 1.2e-2)  # élargir la plage X pour voir le point h=10⁻²
-    plt.ylim(1e-13, 1e-5)  # élargir verticalement pour voir le point h=10⁻²  # étendre vers le bas pour voir le point h=1e-5
-    plt.xticks([1e-5, 1e-4, 1e-3, 1e-2],
-               ['10$^{-5}$', '10$^{-4}$', '10$^{-3}$', '10$^{-2}$'])
-    plt.yticks([1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7],
-               ['10$^{-12}$', '10$^{-11}$', '10$^{-10}$', '10$^{-9}$', '10$^{-8}$', '10$^{-7}$'])
+    plt.xlim(1e-5, 1.2e-2)
+    plt.ylim(1e-13, 1e-5)
+    plt.xticks([1e-5, 1e-4, 1e-3, 1e-2], ['10$^{-5}$', '10$^{-4}$', '10$^{-3}$', '10$^{-2}$'])
+    plt.yticks([1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5], ['10$^{-12}$', '10$^{-11}$', '10$^{-10}$', '10$^{-9}$', '10$^{-8}$', '10$^{-7}$', '10$^{-6}$', '10${-5}$'])
     plt.minorticks_on()
     plt.grid(which='major', linestyle='--', linewidth=0.7, alpha=0.7)
     plt.grid(which='minor', linestyle=':', linewidth=0.3, alpha=0.5)
